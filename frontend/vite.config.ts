43ea6405b2f6559e3ab9ed365f8de74c6e79511e
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2025-08-07 00:43:01 +0200

    feat: add tailwindcss

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,7 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	build: {
-		rollupOptions: {
-			// Force Rollup to use WASM instead of native binaries
-		},
-	},
-	plugins: [react()],
-	// Override Rollup resolution to use WASM
-	resolve: {
-		alias: {
-			rollup: "@rollup/wasm-node",
-		},
-	},
+	plugins: [react(), tailwindcss()],
 });
