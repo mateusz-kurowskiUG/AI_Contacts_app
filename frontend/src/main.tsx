5fc819953a947d7a22265d7ffb73904e16e8b9c9
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2025-08-07 00:53:41 +0200

    feat: add react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
 
+// biome-ignore lint/style/noNonNullAssertion: we know root exists
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
 		<App />
