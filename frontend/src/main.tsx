d99bcbd0d4360fff19d7c8ed35fa68968667d468
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2025-08-06 21:20:49 +0200

    fix: linter errors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+// biome-ignore lint/style/noNonNullAssertion: vite templates require a non-null assertion here
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<App />
