116df79a5c523a0972a32a1a7f5175aec4dc7c20
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2025-08-07 00:51:26 +0200

    feat: add react router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
 
-const root = document.getElementById("root");
-
-ReactDOM.createRoot(root!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>,
