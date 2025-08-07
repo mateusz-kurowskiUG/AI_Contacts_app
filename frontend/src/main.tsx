cf3a97255505f62ca2b11854ae25fdf93763b0c4
Mateusz Kurowski <mateusz.kurowski28@gmail.com>
2025-08-07 19:14:31 +0200

    feat: split CI into separate files, add routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App";
 
 // biome-ignore lint/style/noNonNullAssertion: we know root exists
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
-		<App />
+		<Routes>
+			<Route element={<App />} path="/" />
+		</Routes>
 	</BrowserRouter>,
 );
